8d2c757675f232e54d0234d5f2d09e55b45e7cde
KishanGowdaNK <161239435+KishanGowdaNK@users.noreply.github.com>
2024-02-25 23:13:42 +0530

    Dice_Game
    This is a simple game tour.
    
    While you run the game you will get two dice which are nothing but two players.
    
    When you click on "Start Game" , it will start the game instantly

---
+++ script.js
@@ -0,0 +1,23 @@
+function startGame() {
+    var player1 = (Math.floor((Math.random() * 6) + 1));
+    var player2 = (Math.floor((Math.random() * 6) + 1));
+
+    var player1Img = `./assest/dice${player1}.jpg`;
+    var player2Img = `./assest/dice${player2}.jpg`;
+
+    var result = document.getElementById("result");
+    var elem = document.querySelectorAll('.dice');
+    console.log(elem)
+    elem[0].setAttribute('src', player1Img)
+    elem[1].setAttribute('src', player2Img)
+
+    if (player1 > player2) {
+        result.innerHTML = `player 1 won the match`
+    }
+    else if (player2 > player1) {
+        result.innerHTML = `player 2 won the match`
+    } else {
+        result.innerHTML = `Draw match`
+
+    }
+}
